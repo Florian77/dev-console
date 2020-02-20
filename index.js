ab1db27d76bd5432918636fd4f61fe9477282910
Florian77 <info@ftws.de>
2020-02-21 00:03:30 +0100

    add ramda debug

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // const dc = (...args) => ("DEV_CONSOLE_ON" in process.env) ? R.isEmpty(args) ? true :  console.log(...args) : false;
 // const dcOr = (DEV, LIVE) => dc() ? DEV : LIVE;
 // dc() && console.log("DEV CONSOLE is ON");
@@ -13,7 +12,11 @@
 const isOn = () => ("DEV_CONSOLE_ON" in process.env);
 const showMessageIsOn = () => isOn() ? l("DEV CONSOLE is ON") : "";
 
-const l = console.log.bind(console);
+const l = (...args) => {
+    if (isOn()) {
+        console.log(...args)
+    }
+};
 
 const stringify = v => JSON.stringify(v, null, 2);
 const j = (d, name = false) =>
@@ -21,6 +24,18 @@ const j = (d, name = false) =>
         ? l(stringify(d))
         : l(name, stringify(d))
 ;
+
+
+const r = (name = "") => d => {
+    l(String(name), stringify(d));
+    return d;
+};
+
+const r1 = r("STAGE1:");
+const r2 = r("STAGE2:");
+const r3 = r("STAGE3:");
+const r4 = r("STAGE4:");
+const r5 = r("STAGE5:");
 
 const or = (DEV, LIVE) => isOn() ? DEV : LIVE;
 
@@ -31,6 +46,12 @@ module.exports = {
     isOn,
     l,
     j,
+    r,
+    r1,
+    r2,
+    r3,
+    r4,
+    r5,
     or,
     DEV,
     LIVE,
