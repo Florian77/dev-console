9198103bf749c9f653c6654c71f9d14c6dad9600
Florian <info@ftws.de>
2020-04-18 16:29:23 +0200

    create test + add test script

--- test/index.test.js
+++ test/index.test.js
@@ -1,36 +1,186 @@
-process.env.DEV_CONSOLE_ON = "1";
-
+const expect = require('chai').expect;
 const lib = require("../index");
 
-delete process.env.DEV_CONSOLE_ON;
-console.log(lib.evaluateEnv());
+const log = (...args) => {
+    if (typeof process.env.CONSOLE_LOG_OFF === "undefined") {
+        console.log(...args);
+    }
+};
 
-process.env.DEV_CONSOLE_ON = "1";
-console.log(lib.evaluateEnv());
+describe('reEvaluateEnv', function () {
 
-process.env.DEV_CONSOLE_ON = "0";
-console.log(lib.evaluateEnv());
+    // -----------------------------------------------------------------------------------------------------------------------------
+    beforeEach(function () {
+        log("---------------", this.currentTest.title, "---------------");
+        delete process.env.DEV_CONSOLE_ON;
+        delete process.env.DEV_CONSOLE_FORCE_OFF;
+    });
 
-process.env.DEV_CONSOLE_ON = 1;
-console.log(lib.evaluateEnv());
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('DEV_CONSOLE_ON = 1', function () {
+        process.env.DEV_CONSOLE_ON = 1;
+        log("DEV_CONSOLE_ON [%s] type [%s]", process.env.DEV_CONSOLE_ON, typeof process.env.DEV_CONSOLE_ON);
 
-process.env.DEV_CONSOLE_ON = 0;
-console.log(lib.evaluateEnv());
+        lib.reEvaluateEnv();
+        log("isOn [%s]", lib.isOn());
 
-process.env.DEV_CONSOLE_ON = "STRING-VALUE";
-console.log(lib.evaluateEnv());
+        expect(lib.isOn()).to.be.true;
+    });
 
-// lib.on();
-// lib.off();
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('DEV_CONSOLE_ON = [1]', function () {
+        process.env.DEV_CONSOLE_ON = "1";
+        log("DEV_CONSOLE_ON [%s] type [%s]", process.env.DEV_CONSOLE_ON, typeof process.env.DEV_CONSOLE_ON);
 
-// process.env.DEV_CONSOLE_ON = 0;
-// lib.activate();
-// console.assert(lib.isOn(), "ERROR");
+        lib.reEvaluateEnv();
+        log("isOn [%s]", lib.isOn());
 
-// lib.deactivate();
-// process.env.DEV_CONSOLE_ON = 0;
-// console.log(process.env.DEV_CONSOLE_ON, lib.isOn());
-console.log("[isOn()=%s]", lib.isOn());
-// console.assert(!lib.isOn(), "ERROR");
+        expect(lib.isOn()).to.be.true;
+    });
 
-lib.showMessageIsOn();
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('DEV_CONSOLE_ON = [ 1 ]', function () {
+        process.env.DEV_CONSOLE_ON = " 1 ";
+        log("DEV_CONSOLE_ON [%s] type [%s]", process.env.DEV_CONSOLE_ON, typeof process.env.DEV_CONSOLE_ON);
+
+        lib.reEvaluateEnv();
+        log("isOn [%s]", lib.isOn());
+
+        expect(lib.isOn()).to.be.true;
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('DEV_CONSOLE_ON = 0', function () {
+        process.env.DEV_CONSOLE_ON = 0;
+        log("DEV_CONSOLE_ON [%s] type [%s]", process.env.DEV_CONSOLE_ON, typeof process.env.DEV_CONSOLE_ON);
+
+        lib.reEvaluateEnv();
+        log("isOn [%s]", lib.isOn());
+
+        expect(lib.isOn()).to.be.false;
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('DEV_CONSOLE_ON = [0]', function () {
+        process.env.DEV_CONSOLE_ON = "0";
+        log("DEV_CONSOLE_ON [%s] type [%s]", process.env.DEV_CONSOLE_ON, typeof process.env.DEV_CONSOLE_ON);
+
+        lib.reEvaluateEnv();
+        log("isOn [%s]", lib.isOn());
+
+        expect(lib.isOn()).to.be.false;
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('DEV_CONSOLE_ON = [ 0 ]', function () {
+        process.env.DEV_CONSOLE_ON = " 0 ";
+        log("DEV_CONSOLE_ON [%s] type [%s]", process.env.DEV_CONSOLE_ON, typeof process.env.DEV_CONSOLE_ON);
+
+        lib.reEvaluateEnv();
+        log("isOn [%s]", lib.isOn());
+
+        expect(lib.isOn()).to.be.false;
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('DEV_CONSOLE_ON = []', function () {
+        process.env.DEV_CONSOLE_ON = "";
+        log("DEV_CONSOLE_ON [%s] type [%s]", process.env.DEV_CONSOLE_ON, typeof process.env.DEV_CONSOLE_ON);
+
+        lib.reEvaluateEnv();
+        log("isOn [%s]", lib.isOn());
+
+        expect(lib.isOn()).to.be.false;
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('DEV_CONSOLE_ON = [  ]', function () {
+        process.env.DEV_CONSOLE_ON = "  ";
+        log("DEV_CONSOLE_ON [%s] type [%s]", process.env.DEV_CONSOLE_ON, typeof process.env.DEV_CONSOLE_ON);
+
+        lib.reEvaluateEnv();
+        log("isOn [%s]", lib.isOn());
+
+        expect(lib.isOn()).to.be.false;
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('DEV_CONSOLE_ON = [ random string ]', function () {
+        process.env.DEV_CONSOLE_ON = " random string ";
+        log("DEV_CONSOLE_ON [%s] type [%s]", process.env.DEV_CONSOLE_ON, typeof process.env.DEV_CONSOLE_ON);
+
+        lib.reEvaluateEnv();
+        log("isOn [%s]", lib.isOn());
+
+        expect(lib.isOn()).to.be.false;
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('DEV_CONSOLE_ON undefined + DEV_CONSOLE_FORCE_OFF undefined', function () {
+        log("DEV_CONSOLE_ON [%s] type [%s]", process.env.DEV_CONSOLE_ON, typeof process.env.DEV_CONSOLE_ON);
+        log("DEV_CONSOLE_FORCE_OFF [%s] type [%s]", process.env.DEV_CONSOLE_FORCE_OFF, typeof process.env.DEV_CONSOLE_FORCE_OFF);
+
+        lib.reEvaluateEnv();
+        log("isOn [%s]", lib.isOn());
+
+        expect(lib.isOn()).to.be.false;
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('DEV_CONSOLE_ON = [1] + DEV_CONSOLE_FORCE_OFF = [1]', function () {
+        process.env.DEV_CONSOLE_ON = "1";
+        process.env.DEV_CONSOLE_FORCE_OFF = "1";
+        log("DEV_CONSOLE_ON [%s] type [%s]", process.env.DEV_CONSOLE_ON, typeof process.env.DEV_CONSOLE_ON);
+        log("DEV_CONSOLE_FORCE_OFF [%s] type [%s]", process.env.DEV_CONSOLE_FORCE_OFF, typeof process.env.DEV_CONSOLE_FORCE_OFF);
+
+        lib.reEvaluateEnv();
+        log("isOn [%s]", lib.isOn());
+
+        expect(lib.isOn()).to.be.false;
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('DEV_CONSOLE_ON = [1] + DEV_CONSOLE_FORCE_OFF = [ 1 ]', function () {
+        process.env.DEV_CONSOLE_ON = "1";
+        process.env.DEV_CONSOLE_FORCE_OFF = " 1 ";
+        log("DEV_CONSOLE_ON [%s] type [%s]", process.env.DEV_CONSOLE_ON, typeof process.env.DEV_CONSOLE_ON);
+        log("DEV_CONSOLE_FORCE_OFF [%s] type [%s]", process.env.DEV_CONSOLE_FORCE_OFF, typeof process.env.DEV_CONSOLE_FORCE_OFF);
+
+        lib.reEvaluateEnv();
+        log("isOn [%s]", lib.isOn());
+
+        expect(lib.isOn()).to.be.false;
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('DEV_CONSOLE_ON = [1] + DEV_CONSOLE_FORCE_OFF = [0]', function () {
+        process.env.DEV_CONSOLE_ON = "1";
+        process.env.DEV_CONSOLE_FORCE_OFF = "0";
+        log("DEV_CONSOLE_ON [%s] type [%s]", process.env.DEV_CONSOLE_ON, typeof process.env.DEV_CONSOLE_ON);
+        log("DEV_CONSOLE_FORCE_OFF [%s] type [%s]", process.env.DEV_CONSOLE_FORCE_OFF, typeof process.env.DEV_CONSOLE_FORCE_OFF);
+
+        lib.reEvaluateEnv();
+        log("isOn [%s]", lib.isOn());
+
+        expect(lib.isOn()).to.be.true;
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+    it('DEV_CONSOLE_ON = [1] + DEV_CONSOLE_FORCE_OFF = [ 0 ]', function () {
+        process.env.DEV_CONSOLE_ON = "1";
+        process.env.DEV_CONSOLE_FORCE_OFF = " 0 ";
+        log("DEV_CONSOLE_ON [%s] type [%s]", process.env.DEV_CONSOLE_ON, typeof process.env.DEV_CONSOLE_ON);
+        log("DEV_CONSOLE_FORCE_OFF [%s] type [%s]", process.env.DEV_CONSOLE_FORCE_OFF, typeof process.env.DEV_CONSOLE_FORCE_OFF);
+
+        lib.reEvaluateEnv();
+        log("isOn [%s]", lib.isOn());
+
+        expect(lib.isOn()).to.be.true;
+    });
+
+
+
+});
+
+
+
